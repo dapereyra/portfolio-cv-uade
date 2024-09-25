54f0da21071516b8a5f859aea24d6b8abc6fb4a3
Damián Ezequiel Pereyra <dapereyra@uade.edu.ar>
2024-09-24 21:41:32 -0300

    update cv v2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 document.getElementById('menu-toggle').addEventListener('click', function () {
     document.getElementById('nav-links').classList.toggle('active');
 });
+
+const currentPage = window.location.pathname.split('/').pop();
+const navLinks = document.querySelectorAll('.nav-links a');
+navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPage) {
+        link.classList.add('active');
+    }
+});
